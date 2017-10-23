db72f7a6df63c5e6441d0e3acbb056a82465922e
Glitch (equable-wrinkle) <none>
2017-10-23 21:14:28 +0000

    First commit

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ var forwarded = require('forwarded-for');
 var express = require('express');
 var app = express();
 var port = process.env.PORT;
-
+var osLocale = require('os-locale');
+ var lang;
+osLocale().then(locale => {
+    lang = locale;
+});
 
 app.get('/', function(req,res){
   var address = forwarded(req, req.headers);
+  // var lang = req.headers["accept-language"];
    res.json({
      "ipaddress": address.ip,
-     
+     "language": lang,
+     "software": process.platform
    });
 });
 
